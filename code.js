43196ec296eda067dc3d4abe2b03b519196edc95
mshabana0 <mahamedshabana70@gmail.com>
2023-01-23 12:12:24 +0200

    add my new fill apple.card

---
+++ code.js
@@ -0,0 +1,7 @@
+let username = "Shabana"
+let passwored =12345
+let user_input =prompt('enter user name ;')
+let passwored_input =prompt('enter passwored')
+if (user_input==username){
+    console.log('Welcome')
+}else{'login end'}
